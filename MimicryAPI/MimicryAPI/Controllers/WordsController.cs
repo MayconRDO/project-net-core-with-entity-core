4786daacb862b47d8049664233c8243256ae168b
Maycon Roberto de Oliveira <maycon.roberto.oliveira@gmail.com>
2021-02-02 23:10:36 -0300

    Conclusão inicial da API e criação do objeto palavra

--- MimicryAPI/MimicryAPI/Controllers/WordsController.cs
+++ MimicryAPI/MimicryAPI/Controllers/WordsController.cs
@@ -4,6 +4,7 @@ using MimicryAPI.Models;
 
 namespace MimicryAPI.Controllers
 {
+    [Route("api/words")]
     public class WordsController : ControllerBase
     {
         private readonly MimicryContext _context;
@@ -12,16 +13,22 @@ namespace MimicryAPI.Controllers
             _context = context;
         }
 
+        [Route("")]
+        [HttpGet]
         public ActionResult Get()
         {
             return Ok(_context.Words);
         }
 
+        [Route("{id}")]
+        [HttpGet]
         public ActionResult Get(int id)
         {
             return Ok(_context.Words.Find(id));
         }
 
+        [Route("")]
+        [HttpPost]
         public ActionResult Add(Word word)
         {
             _context.Words.Add(word);
@@ -29,6 +36,8 @@ namespace MimicryAPI.Controllers
             return Ok();
         }
 
+        [Route("{id}")]
+        [HttpPut]
         public ActionResult Update(int id, Word word)
         {
             _context.Words.Update(word);
@@ -36,6 +45,8 @@ namespace MimicryAPI.Controllers
             return Ok();
         }
 
+        [Route("{id}")]
+        [HttpDelete]
         public ActionResult Delete(int id)
         {
             _context.Words.Remove(_context.Words.Find(id));
