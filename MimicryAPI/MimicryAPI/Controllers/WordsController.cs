15a9f02c33d53bc58cbd723ea0e47c44aefa9806
Maycon Roberto de Oliveira <maycon.roberto.oliveira@gmail.com>
2021-02-02 22:32:49 -0300

    My first commit

---
+++ MimicryAPI/MimicryAPI/Controllers/WordsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimicryAPI.DataBase;
+using MimicryAPI.Models;
+
+namespace MimicryAPI.Controllers
+{
+    public class WordsController : ControllerBase
+    {
+        private readonly MimicryContext _context;
+        public WordsController(MimicryContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Get()
+        {
+            return Ok(_context.Words);
+        }
+
+        public ActionResult Get(int id)
+        {
+            return Ok(_context.Words.Find(id));
+        }
+
+        public ActionResult Add(Word word)
+        {
+            _context.Words.Add(word);
+
+            return Ok();
+        }
+
+        public ActionResult Update(int id, Word word)
+        {
+            _context.Words.Update(word);
+
+            return Ok();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            _context.Words.Remove(_context.Words.Find(id));
+
+            return Ok();
+        }
+
+    }
+}
---
+++ MimicryAPI/MimicryAPI/DataBase/MimicryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MimicryAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicryAPI.DataBase
+{
+    public class MimicryContext : DbContext
+    {
+        public MimicryContext(DbContextOptions<MimicryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Word> Words { get; set; }
+
+    }
+}
